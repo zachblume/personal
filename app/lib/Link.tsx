fa8a9caa59ae668d49fda53cf251776069257b37
Zach Blume <zachblume@gmail.com>
2025-01-27 23:03:55 +0000

    prettier

---
+++ app/lib/Link.tsx
@@ -0,0 +1,41 @@
+"use client";
+import { Link as NextLink, useTransitionRouter } from "next-view-transitions";
+
+function Link({ href, children, ...props }) {
+    const router = useTransitionRouter();
+    const isBlogLink = href.includes("/blog/");
+    const className = isBlogLink ? "visited-blog-link" : "";
+
+    return (
+        <span className={className}>
+            <NextLink
+                onMouseDown={(e) => {
+                    // If it is not a left click, exit
+                    if (e.button !== 0) return;
+
+                    // If the command key is held, let the link open in a new tab
+                    if (e.metaKey || e.ctrlKey) return;
+
+                    e.preventDefault();
+                    router.push(href);
+                }}
+                onClick={(e) => {
+                    // If it is not a left click, exit
+                    if (e.button !== 0) return;
+
+                    // If the command key is held, let the link open in a new tab
+                    if (e.metaKey || e.ctrlKey) return;
+
+                    e.preventDefault();
+                }}
+                prefetch={true}
+                href={href}
+                {...props}
+            >
+                {children}
+            </NextLink>
+        </span>
+    );
+}
+
+export { Link };
